89b050bff5ee8be618384a14e4f1f9443dbc0a22
coolyangzc <yang-zc17@mails.tsinghua.edu.cn>
2018-07-13 15:02:48 +0800

    Bigrams Calc

---
+++ Corpus/bigram.py
@@ -0,0 +1,70 @@
+import os, re, string
+
+words = {}
+bigrams = {}
+
+
+def read_corpus():
+    corpus_file = "ANC-written-noduplicate+pangram.txt"
+    f = open(corpus_file, "r")
+    lines = f.readlines()
+    for line in lines[:10000]:
+        data = line.split(' ')
+        words[data[0]] = int(data[1])
+    print(len(words))
+    f.close()
+
+
+def process(file_path):
+    f = open(file_path, "rb")
+    raw = f.read()
+    raw = raw.decode('utf-8').lower()
+    cur = ''
+    data = ['#']
+    for ch in raw:
+        if ch.isalpha():
+            cur += ch
+        else:
+            if cur != '':
+                data.append(cur.lower())
+                cur = ''
+            if data[-1] != '#' and ch in string.punctuation or ch.isdigit():
+                data.append('#')
+    for i in range(len(data)):
+        if data[i] != '#' and data[i] not in words:
+            data[i] = 'OOV'
+    for i in range(len(data) - 1):
+        if data[i] != '#' and data[i+1] != '#' and data[i] != 'OOV' and data[i+1] != 'OOV':
+            s = data[i] + ' ' + data[i+1]
+            if s not in bigrams:
+                bigrams[s] = 1
+            else:
+                bigrams[s] += 1
+    f.close()
+
+
+def scan_files():
+    data_dir = "../../OANC-GrAF/data/"
+    cnt = 0
+    for parent, dir_names, file_names in os.walk(data_dir):
+        for filename in file_names:
+            if filename[-4:] == ".txt" and "written_" in parent:
+                #print("filename with full path:" + os.path.join(parent, filename))
+                #print("Processing " + filename)
+                process(os.path.join(parent, filename))
+                cnt += 1
+                if cnt % 100 == 0:
+                    print("Processed " + str(cnt) + " files")
+
+
+def save_bigrams():
+    f = open("bigrams-written.txt", "w")
+    sorted_bigrams = sorted(bigrams.items(), key=lambda d: d[1], reverse=True)
+    for bigram in sorted_bigrams:
+        print(bigram[0], bigram[1], file = f)
+    f.close()
+
+
+read_corpus()
+scan_files()
+save_bigrams()
