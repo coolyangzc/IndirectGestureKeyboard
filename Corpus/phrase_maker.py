c8e3e5ea86c1fd13300c86f85b4ae05fde56575f
coolyangzc <yang-zc17@mails.tsinghua.edu.cn>
2018-08-02 15:11:53 +0800

    Add phrase_maker

---
+++ Corpus/phrase_maker.py
@@ -0,0 +1,25 @@
+dict_fd = open('ANC-written-noduplicate+pangram.txt', 'rb')
+
+
+dict = {}
+lines = dict_fd.readlines()
+for line in lines[:10000]:
+    line = line.decode('utf-8')
+    word = line.split(' ')[0]
+    dict[word] = 1
+
+phrase_fd = open('phrases.txt', 'rb')
+outfd = open('pangrams_phrases.txt', 'w')
+lines = phrase_fd.readlines()
+print('the quick brown fox jumps over the lazy dog', file=outfd)
+print('the five boxing wizards jump quickly', file=outfd)
+for line in lines:
+    line = line.decode('utf-8').strip().lower()
+    words = line.split(' ')
+    ok = True
+    for word in words:
+        if word not in dict:
+            ok = False
+            break
+    if ok:
+        print(line, file=outfd)
